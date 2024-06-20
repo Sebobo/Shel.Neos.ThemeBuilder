bbfe3b22ea56eab16c3ffa7e5d22e3dfdca28348
Sebastian Helzle <sebastian@helzle.it>
2024-06-20 11:39:30 +0200

    FEATURE: Make it possible to show unset theme variables in datasource

--- Classes/DataSource/ThemeColorsDataSource.php
+++ Classes/DataSource/ThemeColorsDataSource.php
@@ -42,6 +42,7 @@ class ThemeColorsDataSource extends AbstractDataSource
     /**
      * Render each palette property of the nodetype "Shel.Neos.ThemeBuilder:Mixin.PageTheme" as selectable color/value with a preview
      *
+     * @param array{filterUnsetProperties?: bool} $arguments
      * @throws Exception|NodeException|NodeTypeNotFoundException
      */
     public function getData(
@@ -51,6 +52,8 @@ class ThemeColorsDataSource extends AbstractDataSource
         if (!$node) {
             return [];
         }
+
+        $filterUnsetProperties = ($arguments['filterUnsetProperties'] ?? true) !== 'false';
 
         /** @var ContentContext $context */
         $context = $node->getContext();
@@ -65,7 +68,13 @@ class ThemeColorsDataSource extends AbstractDataSource
 
         return array_filter(
             array_map(
-                function (string $propertyName) use ($closestThemedNode, $siteNode, $themeProperties, $groups) {
+                function (string $propertyName) use (
+                    $closestThemedNode,
+                    $siteNode,
+                    $themeProperties,
+                    $groups,
+                    $filterUnsetProperties
+                ) {
                     // Hide non-color properties
                     if (!ThemePropertyHelper::isColorProperty(
                         $propertyName,
@@ -79,9 +88,10 @@ class ThemeColorsDataSource extends AbstractDataSource
                         $closestThemedNode,
                         $siteNode
                     );
-                    if (!$value) {
+                    if (!$value && $filterUnsetProperties) {
                         return null;
                     }
+
                     // Return color property of closest themed node
                     return $this->createThemeColorOption(
                         $propertyName,
@@ -118,9 +128,17 @@ class ThemeColorsDataSource extends AbstractDataSource
             'label' => $label,
             'value' => 'var(--' . $value . ')',
             'group' => $group,
-            // TODO: Render SVG preview only for colors
-            'preview' => 'data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="' . $propertyValue . '" d="M0 96C0 60.7 28.7 32 64 32H384c35.3 0 64 28.7 64 64V416c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V96z"/></svg>'
+            'preview' => $this->getColorPreview($propertyValue)
         ];
+    }
+
+    protected function getColorPreview(?string $color): string
+    {
+        if ($color) {
+            // TODO: Render SVG preview only for colors
+            return 'data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="' . $color . '" d="M0 96C0 60.7 28.7 32 64 32H384c35.3 0 64 28.7 64 64V416c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V96z"/></svg>';
+        }
+        return 'data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M504 256c0 137-111 248-248 248S8 393 8 256C8 119.1 119 8 256 8s248 111.1 248 248zM262.7 90c-54.5 0-89.3 23-116.5 63.8-3.5 5.3-2.4 12.4 2.7 16.3l34.7 26.3c5.2 3.9 12.6 3 16.7-2.1 17.9-22.7 30.1-35.8 57.3-35.8 20.4 0 45.7 13.1 45.7 33 0 15-12.4 22.7-32.5 34C247.1 238.5 216 254.9 216 296v4c0 6.6 5.4 12 12 12h56c6.6 0 12-5.4 12-12v-1.3c0-28.5 83.2-29.6 83.2-106.7 0-58-60.2-102-116.5-102zM256 338c-25.4 0-46 20.6-46 46 0 25.4 20.6 46 46 46s46-20.6 46-46c0-25.4-20.6-46-46-46z"/></svg>';
     }
 
     protected function getGroupLabel(string $propertyName, array $themeProperties, array $groups): string
